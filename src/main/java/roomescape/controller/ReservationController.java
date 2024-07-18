192cb01b55d6eae940f45e7eca65225b36682421
dyrlqhffo <dyrlqhffo@gmail.com>
2024-07-19 01:01:28 +0900

    2-2 로그인 리팩터링 예약 생성 기능 변경 - 사용자 완료

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -4,6 +4,8 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import roomescape.argumentresolver.LoginUser;
+import roomescape.domain.User;
 import roomescape.dto.reservation.ReservationsResponse;
 import roomescape.dto.reservation.create.ReservationCreateRequest;
 import roomescape.dto.reservation.create.ReservationCreateResponse;
@@ -30,7 +32,9 @@ public class ReservationController {
     }
 
     @PostMapping
-    public ResponseEntity<ReservationCreateResponse> create(@Valid @RequestBody ReservationCreateRequest dto) {
+    public ResponseEntity<ReservationCreateResponse> create(@Valid @RequestBody ReservationCreateRequest dto,
+                                                            @LoginUser User user) {
+        dto.addName(user.getName());
         ReservationCreateResponse reservation = reservationService.createReservation(dto);
         return ResponseEntity.ok().body(reservation);
     }
--- src/main/java/roomescape/dto/reservation/create/ReservationCreateRequest.java
+++ src/main/java/roomescape/dto/reservation/create/ReservationCreateRequest.java
@@ -1,12 +1,13 @@
 package roomescape.dto.reservation.create;
 
 import jakarta.validation.constraints.*;
+import roomescape.domain.User;
 
 public class ReservationCreateRequest {
 
     @NotBlank(message = "날짜를 제대로 입력해주세요.")
     private String date;
-    @NotBlank(message = "예약자는 필수항목입니다.")
+//    @NotBlank(message = "예약자는 필수항목입니다.")
     private String name;
 
     @NotNull(message = "시간을 선택해주세요.")
@@ -14,6 +15,10 @@ public class ReservationCreateRequest {
 
     @NotNull(message = "테마을 선택해주세요.")
     private Long themeId;
+
+    public void addName(String userName) {
+        this.name = userName;
+    }
 
     public ReservationCreateRequest() {
     }
